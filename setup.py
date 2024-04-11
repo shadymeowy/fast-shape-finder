d29b084cbb3d21b5cd5a31691ae3396f53911bbc
shadymeowy <tolgademirdal@gmail.com>
2024-04-11 20:28:40 +0300

    switch to pyproject.toml and use a proper build config

--- setup.py
+++ setup.py
@@ -1,25 +1,15 @@
 from setuptools import setup
 from setuptools.extension import Extension
-from Cython.Build import cythonize
 
-cc_flags = ['-O3', '-march=native', '-mtune=native', '-fopenmp']
-# cc_flags = ['-O2']
+cc_flags = ['-O3', '-march=native', '-mtune=native']
 
-modules = cythonize([
+modules = [
     Extension(
-        "fast_shape_finder", ["fast_shape_finder.pyx"],
+        name="fast_shape_finder",
+        sources=["fast_shape_finder.pyx"],
         extra_compile_args=cc_flags,
         extra_link_args=cc_flags
     ),
-])
+]
 
-setup(
-    name='fast-shape-finder',
-    version='1.0.0a1',
-    description='Finding simple geometric shapes quickly using RANSAC-based algorithms',
-    author='Tolga Demirdal',
-    url='https://github.com/shadymeowy/fast-shape-finder',
-    setup_requires=["cython"],
-    install_requires=[],
-    ext_modules=modules
-)
+setup(ext_modules=modules)
